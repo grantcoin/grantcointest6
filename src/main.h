b57d23bb9aa7be827d2a5f658eaec6b809650f59
grantcoin <mail@grantcoin.org>
2015-03-07 05:28:11 -0500

    Update main.h

--- src/main.h
+++ src/main.h
@@ -41,7 +41,7 @@ inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONE
 static const int COINBASE_MATURITY_GRT = 500;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
-static const int STAKE_TARGET_SPACING = 90; // 90-second block spacing 
+static const int STAKE_TARGET_SPACING = 1.5 * 60; // 90-second block spacing 
 static const int STAKE_MIN_AGE = 60 * 60 * 24; // minimum age for coin age (24 hours)
 static const int STAKE_MAX_AGE = 60 * 60 * 24 * 90; // stake age of full weight
 
